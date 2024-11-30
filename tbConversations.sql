694abc7723b86755d859d166b7556588d4d15255
betapinet <betapinet@gmail.com>
2024-11-30 02:25:15 +0000

    Delete tbConversations.sql

--- tbConversations.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Conversations (
-    ID INT IDENTITY(1,1) PRIMARY KEY, -- ستون شناسه (کلید اصلی)
-    UserInput NVARCHAR(MAX) NOT NULL, -- ورودی کاربر
-    AssistantResponse NVARCHAR(MAX) NOT NULL, -- پاسخ دستیار
-    Timestamp DATETIME NOT NULL DEFAULT GETDATE() -- زمان مکالمه
-);
